fbcd7687d33e8e2229f1fd67e81bbae9dc83be65
linWeiX <linwx0912@gmail.com>
2022-04-09 10:46:46 +0800

    main

---
+++ linweb/qq.py
@@ -0,0 +1,11 @@
+a = int(input("其中考(40%)："))
+b = int(input("其末考(60%)："))
+sum = (a*0.4)+(b*0.6)
+
+if(0<=sum<60):{print("總分",sum,"E")}
+elif(sum<=70):{print("總分",sum,"D")}
+elif(sum<=80):{print("總分",sum,"C")}
+elif(sum<90):{print("總分",sum,"B")}
+elif(100>=sum>90):{print("總分",sum,"A")}
+else:{print("錯誤囉")}
+"""銘傳程式設計作業其中一題"""
